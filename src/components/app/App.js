35549efae7d9922ebb28191cb15692a9d6917b35
Steven Cheung <stevencheung@hotmail.co.uk>
2022-07-12 19:25:52 +0100

    added components and adding href to each component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,19 +11,24 @@ import LandingPage from "../landingPage/landingPage";
 const App = () => (
   <React.Fragment>
     {/* Start of navbar */}
-    <Menu mode="horizontal" defaultSelectedKeys={["mail"]}>
-      <Menu.Item key="home" icon={<HomeOutlined />}>
-        Home
-      </Menu.Item>
-      <Menu.Item key="contact" icon={<ContactsOutlined />}>
-        Contact me
-      </Menu.Item>
-      <Menu.SubMenu key="SubMenu" title="See more" icon={<MenuOutlined />}>
-        <Menu.Item key="two">About me</Menu.Item>
-        <Menu.Item key="three">Portfolio projects</Menu.Item>
-        <Menu.Item key="four">Tech</Menu.Item>
-      </Menu.SubMenu>
-    </Menu>
+    <div className="navbar">
+      <Menu mode="horizontal" defaultSelectedKeys={["mail"]}>
+        <Menu.Item
+          key="home"
+          icon={<HomeOutlined />}
+          label={<a href=".land-text-container">Home</a>}
+        >
+          Home
+        </Menu.Item>
+        <Menu.SubMenu key="SubMenu" title="See more" icon={<MenuOutlined />}>
+          <Menu.Item key="two">About me</Menu.Item>
+          <Menu.Item key="three">Portfolio projects</Menu.Item>
+          <Menu.Item key="contact" icon={<ContactsOutlined />}>
+            Contact me
+          </Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </div>
     {/* End of navbar */}
     <LandingPage />
   </React.Fragment>
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,20 +1,25 @@
 import "./landingPage.css";
 import React from "react";
+import { Col, Row } from "antd";
 
 function LandingPage() {
   return (
     <React.Fragment>
-      <article className="landing-container">
-        <article className="landing-text-container">
-          <h1> Hi, I'm Steven Cheung</h1>
-          <p>London based, full-stack developer</p>
-        </article>
-        <img
-          className="landing-img"
-          src="https://pbs.twimg.com/media/E610YRMXoAAjvxa?format=jpg&name=large"
-          alt="Landing pic"
-        />
-      </article>
+      <Row>
+        <Col sm={12}>
+          <article className="land-text-container">
+            <h1> Hi, I'm Steven Cheung</h1>
+            <p>London based, full-stack developer</p>
+          </article>
+        </Col>
+        <Col sm={12}>
+          <img
+            className="landing-img"
+            src="https://pbs.twimg.com/media/E610YRMXoAAjvxa?format=jpg&name=large"
+            alt="Landing pic"
+          />
+        </Col>
+      </Row>
     </React.Fragment>
   );
 }
