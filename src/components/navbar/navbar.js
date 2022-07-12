45d6c59ac7d68a959575b784caf5498c3ea5c013
Steven Cheung <stevencheung@hotmail.co.uk>
2022-07-12 21:51:56 +0100

    completed styling of navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,30 +1,48 @@
 import "./navbar.css";
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+
+  function handleClick() {
+    if (!show) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  }
+
   return (
     <React.Fragment>
       <nav>
         <div class="logo">
           <p>S Cheung</p>
         </div>
-        <div class="hamburger">
+        <div class="hamburger" onClick={handleClick}>
           <div class="line1"></div>
           <div class="line2"></div>
           <div class="line3"></div>
         </div>
-        <ul class="nav-links">
+        <ul class={show ? "nav-links open" : "nav-links"}>
           <li>
-            <a href="#landing-container">Home</a>
+            <a href="#landing-container" onClick={handleClick}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about-me-page-container">About me</a>
+            <a href="#about-me-page-container" onClick={handleClick}>
+              About me
+            </a>
           </li>
           <li>
-            <a href="#portfolio-page-container">Portfolio</a>
+            <a href="#portfolio-page-container" onClick={handleClick}>
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="#contact-me-page-container">Contact Me</a>
+            <a href="#contact-me-page-container" onClick={handleClick}>
+              Contact Me
+            </a>
           </li>
         </ul>
       </nav>
