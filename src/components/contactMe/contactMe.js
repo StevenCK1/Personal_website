27160eb2599249c7f4337528c75f48e6d19e7d44
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-03 21:08:04 +0100

    started working on contact me page

--- src/components/contactMe/contactMe.js
+++ src/components/contactMe/contactMe.js
@@ -4,6 +4,32 @@ function ContactMe() {
   return (
     <div className="contact-me-page-container" id="contact-me-page-container">
       <h1>This is the contact-me section</h1>
+      <form name="contact" method="POST" data-netlify="true">
+        <div>
+          <label>
+            First Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </div>
   );
 }
