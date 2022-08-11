58d7bb05139aa3abaf37d9b860d3dbf918cc58af
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-12 00:20:51 +0100

    updated project on portfolio page

--- src/libs/projectItems.js
+++ src/libs/projectItems.js
@@ -1,21 +1,23 @@
 import Shopping_cart from "../images/Shopping_cart.png";
+import Reconnect from "../images/Reconnect.png";
 
 export const items = [
   {
-    href: "https://github.com/StevenCK1/Shopping_cart",
-    src: Shopping_cart,
-    alt: "Shopping Cart",
-    title: "Shopping Cart",
+    href: "https://github.com/SchoolOfCode/final-project_front-end-surch",
+    src: Reconnect,
+    alt: "Reconnect-App",
+    title: "Reconnect App",
     description:
-      "Built a shopping cart using only vanilla JavaScript, HTML and CSS",
+      "Reconnect is a wellness app that aims to help the users to reconnect with themselves by keeping track of their mood and obtain suggestions on how to improve their well being.",
   },
   {
-    href: "https://github.com/StevenCK1/Shopping_cart",
-    src: Shopping_cart,
-    alt: "Shopping Cart",
-    title: "Shopping Cart",
-    description:
-      "Built a shopping cart using only vanilla JavaScript, HTML and CSS",
+    href: "https://github.com/SchoolOfCode/w9_frontend-project-team-js",
+    src: "https://raw.githubusercontent.com/SchoolOfCode/w9_frontend-project-team-js/main/images/skill.JPG",
+    alt: "Backpack App",
+    title: "Backpack App",
+    description: `Backpack is an app that aims to support students learning to code - or learning any skill for that matter!
+
+      The general premise of the app is to help students to keep track of their development, by breaking down the topics into individual skills, as well as tools to assist in improving those skills.`,
   },
   {
     href: "https://github.com/StevenCK1/Shopping_cart",
@@ -34,3 +36,8 @@ export const items = [
       "Built a shopping cart using only vanilla JavaScript, HTML and CSS",
   },
 ];
+// backend of backpack
+// https://github.com/SchoolOfCode/w9_backend-project-team-js
+
+// backend of reconnect
+// https://github.com/SchoolOfCode/final-project_back-end-surch 
