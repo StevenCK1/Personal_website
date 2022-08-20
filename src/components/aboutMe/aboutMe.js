7f20f8d5da6afcac726617be676ac8c523311ae6
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-20 18:03:55 +0100

    minor changes

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -9,17 +9,29 @@ function AboutMe() {
     <div className="about-me-page-container" id="about-me-page-container">
       <h1>A little bit about me</h1>
       <article className="little-about-me">
+        <Row>
+          <Col></Col>
+        </Row>
         <p>
           I come from Mauritius and took a leap of faith to work in finance in
           London, and now I am taking a new leap of faith to follow my passion
-          for tech. I love sports, music and learning about new technologies. To
-          follow this passion for tech, I decided to change my career path and
-          start learning how to code. Currently, I am studying to become a web
-          developer. I was selected to join 200 (out of 1800) other bootcampers
-          at the School of Code full stack web developer bootcamp. The intensive
-          and immersive sixteen-week programming bootcamp focuses on exposing to
-          a range of different industry best practices and concepts, Agile
-          working methodologies, pair programming, and creative problem-solving.
+          for tech.
+        </p>
+        <p>
+          I love sports, music and learning about new technologies. To follow
+          this passion for tech, I decided to change my career path and start
+          learning how to code.
+        </p>
+        <p>
+          I have recently completed the School of Code bootcamp to become a web
+          developer. I was selected to join 200 (out of 3000) other bootcampers
+          at the School of Code full stack web developer bootcamp.
+        </p>
+        <p>
+          The intensive and immersive sixteen-week programming bootcamp focuses
+          on exposing to a range of different industry best practices and
+          concepts, Agile working methodologies, pair programming, and creative
+          problem-solving.
         </p>
         <p>The pen is mightier than the sword, as they say...</p>
         <p>Take away the pen, the keyboard is mightier than the pen!</p>
